5904c83df31ef29b28ccee1c89c91c907d8559b9
Joni Toivonen <toivjoni@gmail.com>
2020-04-13 15:31:15 +0300

    Add PUT method to kong routes in kong-config script

--- kong-config.sh
+++ kong-config.sh
@@ -9,7 +9,7 @@ else
 fi;
 
 $DOCKER exec -it $CONTAINER_ID curl -i -X POST --url http://localhost:8001/services/ --data 'name=album-list' --data 'url=http://resourcestore:3000'
-$DOCKER exec -it $CONTAINER_ID curl -i -X POST --url http://localhost:8001/services/album-list/routes --data 'methods[]=GET&methods[]=POST'
+$DOCKER exec -it $CONTAINER_ID curl -i -X POST --url http://localhost:8001/services/album-list/routes --data 'methods[]=GET&methods[]=POST&methods[]=PUT'
 
 $DOCKER exec -it $CONTAINER_ID curl -i -X POST --url http://localhost:8001/services/album-list/plugins/ --data 'name=key-auth'
 
